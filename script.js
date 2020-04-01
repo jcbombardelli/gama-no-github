67da6e81079f002b2188eda0a0561686fefe35c6
JC Bombardelli <jc.bombardelli@live.com>
2020-04-01 11:07:29 -0300

    Arquivo alterado

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
-function validaCarrinho () {
-  console.log("iniciando validação carrinho")
+function validaCarrinho() {
+  console.log('iniciando validação carrinho');
+}
+
+function log() {
+  console.log('novo log');
 }
